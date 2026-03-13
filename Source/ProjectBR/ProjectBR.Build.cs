45997bf54403bd72903fa2738dbebb40e90fb2e0
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-03-14 00:00:54 +0900

    [Add] : Project 세팅 추가

--- Source/ProjectBR/ProjectBR.Build.cs
+++ Source/ProjectBR/ProjectBR.Build.cs
@@ -8,9 +8,15 @@ public class ProjectBR : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG",  "GameplayAbilities",
+			"GameplayTags",
+			"GameplayTasks",
+			"NavigationSystem",
+			"AIModule",
+			"Niagara"
+		});
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
