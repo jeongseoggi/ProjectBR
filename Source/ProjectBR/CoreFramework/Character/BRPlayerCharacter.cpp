99a18aa81d4c98eadbeb6c55efe3fc0e6e8bbbb6
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-03-14 00:20:54 +0900

    [Add] : PlayerState, PlayerCharacter 클래스 추가

---
+++ Source/ProjectBR/CoreFramework/Character/BRPlayerCharacter.cpp
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BRPlayerCharacter.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+
+ABRPlayerCharacter::ABRPlayerCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void ABRPlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
+void ABRPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	APlayerController* PC = Cast<APlayerController>(GetController());
+	check(PC);
+	
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer()))
+	{
+		if (DefaultMappingContext)
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+	
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		
+	}
+}
+
---
+++ Source/ProjectBR/CoreFramework/Character/BRPlayerCharacter.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BRPlayerCharacter.generated.h"
+
+class UInputMappingContext;
+
+UCLASS()
+class PROJECTBR_API ABRPlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABRPlayerCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TObjectPtr<UInputMappingContext> DefaultMappingContext;
+};
